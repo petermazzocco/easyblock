49faaa1c1734f0e04f686779ab1a32bcdd5759ef
PM <100664895+OhItsPM@users.noreply.github.com>
2023-02-18 17:52:18 -0800

    Updated erorr page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Error = () => {
   return (
-    <div>
-      <h1>Error</h1>
+    <div className="bg-animated bg-fixed bg-cover bg-center h-screen">
+      <h1 className="text-center text-2xl text-white">Error</h1>
+      <p className="text-center text-xl text-white font-thin">
+        Please go back.
+      </p>
     </div>
   );
 };
