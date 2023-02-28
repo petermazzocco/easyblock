2087d1dcc5d9850f6b2401393b442451ca6cd049
PM <100664895+OhItsPM@users.noreply.github.com>
2023-02-28 13:06:57 -0800

    Fixed amount to display in tx

--- src/pages/TransactionInfo.js
+++ src/pages/TransactionInfo.js
@@ -8,40 +8,23 @@ const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
-
 const alchemy = new Alchemy(settings);
 
 const TransactionInfo = () => {
   const { tx } = useParams();
-
-  const [data, setData] = useState();
+  const [data, setData] = useState("");
   useEffect(() => {
     async function getTxData() {
       try {
-        const data = await alchemy.core.getTransactionReceipt(tx, true);
+        const data = await alchemy.core.getTransaction(tx);
         setData(data);
-        console.log("Transaction data is listed.");
-      } catch {
-        console.log("Couldn't get transaction info");
+        // console.log("Transaction data is listed.");
+      } catch (err) {
+        console.log("Couldn't get transaction info, error:", err);
       }
     }
-
     getTxData();
   }, [tx]);
-
-  // const [timestamp, setTimestamp] = useState();
-  // useEffect(() => {
-  //   async function getTimestamp() {
-  //     try {
-  //       const time = await alchemy.core.getBlockWithTransactions(tx);
-  //       setTimestamp(time.timestamp);
-  //       console.log(`Date: ${time.timestamp}`);
-  //     } catch {
-  //       console.log("Invalid Date");
-  //     }
-  //   }
-  //   getTimestamp();
-  // }, []);
 
   return (
     <div className="bg-animated bg-fixed bg-center bg-cover h-screen">
@@ -50,7 +33,7 @@ const TransactionInfo = () => {
           Transaction Details
         </h1>
         <p className="font-thin md:text-xl sm:text-md xs:text-xs italic pt-2">
-          {tx}
+          {tx.slice(0, 2) + "..." + tx.slice(50)}
         </p>
       </div>
       {!data ? (
@@ -70,10 +53,10 @@ const TransactionInfo = () => {
                 <p className="sm:text-2xl  xs:text-lg font-bold">
                   Transaction Hash
                 </p>
-                <p className="sm:text-md xs:text-xs md:text-xl">{tx}</p>
+                <p className="sm:text-md xs:text-xs md:text-xl">
+                  {tx.slice(0, 2) + "..." + tx.slice(50)}
+                </p>
               </motion.div>
-              {/* <p className="text-2xl font-bold">Timestamp</p>
-              <p>{new Date(timestamp * 1000).toLocaleString()}</p> */}
               <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
@@ -81,7 +64,9 @@ const TransactionInfo = () => {
                 transition={{ ease: "easeOut", duration: 0.2, delay: 0.4 }}
               >
                 <p className="sm:text-2xl xs:text-lg font-bold">Amount</p>
-                <p className="sm:text-md xs:text-xs md:text-xl">{data.value}</p>
+                <p className="sm:text-md xs:text-xs md:text-xl">
+                  {data.value.toLocaleString() / 10 ** 18} ETH
+                </p>
               </motion.div>
               <motion.div
                 initial={{ opacity: 0 }}
@@ -109,41 +94,11 @@ const TransactionInfo = () => {
               >
                 <p className="sm:text-2xl xs:text-lg font-bold">Gas Used</p>
                 <p className="sm:text-md xs:text-xs md:text-xl">
-                  {data.gasUsed.toString()}
+                  {data.gasUsed}
                 </p>
               </motion.div>
             </div>
           </div>
-          {/* <div className="mx-20 text-white mt-20 ">
-            <h2 className="font-bold text-xl">What Does It Mean?</h2>
-            <p>
-              Let's dive into what each of these mean <br></br>
-              <span className="font-black underline">
-                Transaction Hash:
-              </span>{" "}
-              You know how when you get a long CVS receipt and it has a order
-              number on it? Consider this the order number of the receipt.{" "}
-              <br></br>
-              <span className="font-black underline">Amount:</span> This is
-              pretty self explanitory, but it's the amount that was sent in ETH
-              to either another person or a smart contract. <br></br>
-              <span className="font-black underline">
-                Sender & Receiver:
-              </span>{" "}
-              This is who sent, or approved, the transaction, and who, or what,
-              recieved the transaction. Receivers can either be another
-              externally owned account like another persons wallet, or it could
-              be a smart contract, like buying an NFT. <br></br>
-              <span className="font-black underline">Nonce:</span> Now this is a
-              little tricky. Simply put, <span className="italic">nonce's</span>{" "}
-              are a way to prevent duplicate transactions. It tells the miner
-              that it's a new transaction and it gets pushed through. This is
-              crucial for security!<br></br>
-              <span className="font-black underline">Gas Used:</span> Look at
-              this as the fee for the transaction. Each transaction comes with a
-              fee to the network.<br></br>
-            </p>
-          </div> */}
         </>
       )}
     </div>
